349e95e5fac5fc05b77663f16d08636c99c0161b
Ryan Berry <rberry16@hotmail.com>
2022-06-10 10:00:56 -0700

    all middleware and routers complete

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to actions
+const Act = require('./actions-model');
+const Pro = require('../projects/projects-model');
+
+async function checkId(req, res, next) {
+    const action = await Act.get(req.params.id);
+    if (!action) {
+        res.status(400).json({message: 'no action with that id found'});
+    } else {
+        next();
+    }
+}
+
+async function checkPost(req, res, next) {
+    if (!req.body.description || req.body.description === undefined ||
+        !req.body.notes || req.body.notes === undefined) {
+            res.status(400).json({message: 'description and notes are required'});
+        } else {
+            next();
+        }
+}
+
+async function checkProject_id(req, res, next) {
+    const project = await Pro.get(req.body.project_id);
+    if (!project) {
+        res.status(404).json({message: 'no project with that project_id found'});
+    } else {
+        next();
+    }
+}
+
+module.exports = {
+    checkId,
+    checkPost,
+    checkProject_id
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,9 +5,34 @@ const Act = require('./actions-model');
 
 const router = express.Router();
 
+//gets all actions
 router.get('/actions', async (req, res) => {
     const actions = await Act.get();
     res.json(actions);
+});
+
+//gets action by id
+router.get('/actions/:id', md.checkId, async (req, res) => {
+    const action = await Act.get(req.params.id);
+    res.json(action);
+});
+
+//posts new action
+router.post('/actions', md.checkPost, md.checkProject_id, async (req, res) => {
+    const newAction = await Act.insert(req.body);
+    res.status(201).json(newAction);
+});
+
+//updates existing action
+router.put('/actions/:id',md.checkId, md.checkPost, async (req, res) => {
+    const updatedAction = await Act.update(req.params.id, req.body);
+    res.json(updatedAction);
 })
+
+//deletes existing action
+router.delete('/actions/:id', md.checkId, async (req, res) => {
+    await Act.remove(req.params.id);
+    res.json();
+});
 
 module.exports = router;
